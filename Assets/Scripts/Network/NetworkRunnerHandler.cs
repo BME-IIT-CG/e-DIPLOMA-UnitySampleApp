afcbd0af954693542fecf795a413b425d3f6e239
hbede <benedekhorvath@edu.bme.hu>
2023-11-15 00:35:58 +0100

    added more files

---
+++ Assets/Scripts/Network/NetworkRunnerHandler.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkRunnerHandler : MonoBehaviour
+{
+    public NetworkRunner networkRunnerPrefab;
+
+    private NetworkRunner _networkRunner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _networkRunner = Instantiate(networkRunnerPrefab);
+        _networkRunner.name = "Network runner";
+
+        var clientTask = InitNetworkRunner(_networkRunner, GameMode.AutoHostOrClient, NetAddress.Any(),
+            SceneManager.GetActiveScene().buildIndex, null);
+        Debug.Log("Server NetworkRunner started");
+    }
+
+    protected virtual Task InitNetworkRunner(NetworkRunner runner, GameMode gameMode, NetAddress address,
+        SceneRef scene, Action<NetworkRunner> initialized)
+    {
+        var sceneManager = runner.GetComponents(typeof(MonoBehaviour)).OfType<INetworkSceneManager>().FirstOrDefault();
+
+        if (sceneManager == null)
+        {
+            sceneManager = runner.gameObject.AddComponent<NetworkSceneManagerDefault>();
+        }
+
+        runner.ProvideInput = true;
+
+        return runner.StartGame(new StartGameArgs
+        {
+            GameMode = gameMode,
+            Address = address,
+            Scene = scene,
+            SessionName = "TestRoom",
+            Initialized = initialized,
+            SceneManager = sceneManager
+        });
+    }
+}
